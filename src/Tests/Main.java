71302f2714dd9829b943a662d02c22347dd012c0
LumenWang <lumenwang@163.com>
2019-11-26 14:39:50 +0800

    Initial commit

---
+++ src/Tests/Main.java
@@ -0,0 +1,12 @@
+package Tests;
+
+/**
+ * @Author: Lumen Wang https://www.zhihu.com/people/dai-li-72-46/activities
+ * @Date: 2019-11-26 14:38
+ * @Version: 1.0
+ */
+public class Main {
+    public static void main(String[] args){
+        System.out.println("hello");
+    }
+}
